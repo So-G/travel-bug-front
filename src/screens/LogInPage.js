bd0b99ad461eacac90562cd6a321db94bcd73e7f
Solene G <solene.gerard@gmail.com>
2021-07-27 18:12:20 +0200

    added react hook form

--- src/screens/LogInPage.js
+++ src/screens/LogInPage.js
@@ -1,8 +1,11 @@
+import LogInForm from '../components/LogInForm';
+
 export default function LogIn() {
   return (
     <>
       <h4>Log In Page</h4>
       <p>Coming later this summer...</p>
+      <LogInForm />
     </>
   );
 }
